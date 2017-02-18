714eeb3e1466a7cafe8e74d6f0df0c0aefdb35e9
Krystal Bernal <krystalbc@sbcglobal.net>
2017-02-17 18:35:28 -0800

    Add files via upload

---
+++ FUNdamentals_Day3-RGB_LED.ino
@@ -0,0 +1,68 @@
+/*Krystal Bernal
+ * Engineering FUNdamentals
+ * Day 3: RGB LED
+ * This program teaches for statements 
+ * to control the color of an RGB LED
+ * last updated 4/9/16
+*/
+
+const int redPin = 3;
+const int greenPin = 5;
+const int bluePin = 6;
+
+int redValue;
+int greenValue;
+int blueValue;
+
+void setup(){
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+  digitalWrite(redPin, LOW);
+  digitalWrite(greenPin, LOW);
+  digitalWrite(bluePin, LOW);
+}
+
+
+
+void loop(){
+  redValue = 255; // choose a value between 1 and 255 to change the color.
+  greenValue = 0;
+  blueValue = 0;
+
+  analogWrite(redPin, 0);
+  delay(5000);
+
+   for(int i = 0; i < 255; i++){
+    redValue--;
+    greenValue++;
+    analogWrite(redPin, 255 - redValue);
+    analogWrite(greenPin, 255 - greenValue);
+    delay(50);
+  }
+
+  redValue = 0;
+  greenValue = 255;
+  blueValue = 0;
+
+  for(int i = 0; i < 255; i++){
+    greenValue--;
+    blueValue++;
+    analogWrite(greenPin, 255 - greenValue);
+    analogWrite(bluePin, 255 - blueValue);
+    delay(50);
+  }
+
+  redValue = 0;
+  greenValue = 0;
+  blueValue = 255;
+
+
+  for(int i = 0; i < 255; i++){
+  redValue++;
+  blueValue--;
+  analogWrite(redPin, 255 - redValue);
+  analogWrite(bluePin, 255 - blueValue);
+  delay(50);
+  }
+}
---
+++ FUNdamentals_Day4-Servo_Routine.ino
@@ -0,0 +1,57 @@
+/*
+ * Krystal Bernal
+ * Engineering FUNdametals
+ * Day 4: Servo Routine
+ * This program uses 2 Continuous Rotation Servos.
+ * Last updated: 3/25/16
+*
+ * Parallax motor specs
+ * 180: Turn clockwise
+ * 90: Stops the motor
+ * 0: Turn counterclockwise
+*/
+
+#include <Servo.h> //include Servo library
+
+Servo leftMotor;
+Servo rightMotor; //declare motors
+
+const int Lforward = 0;
+const int Rforward = 180;
+
+const int right = 180;
+const int left = 0;
+const int neutral = 90;
+
+void setup() {
+  Serial.begin(9600);
+  
+  rightMotor.attach(9);               //send signals to right motor through pin 5
+  leftMotor.attach(10);               //send signals to left motor through pin 6
+  rightMotor.write(neutral);          //set the motors to neutral
+  leftMotor.write(neutral);
+}
+
+void loop() {
+    
+    rightMotor.write(Rforward);
+    leftMotor.write(Lforward);
+    delay(3000);
+    
+    rightMotor.write(neutral);
+    leftMotor.write(neutral);
+    delay(1000);
+    
+    rightMotor.write(right);  // turn right
+    leftMotor.write(right);
+    delay(500);
+
+    rightMotor.write(left);   // turn left
+    leftMotor.write(left);
+    delay(750);
+
+    rightMotor.write(right);  // reset facing forward
+    leftMotor.write(right);
+    delay(500);
+    
+}
