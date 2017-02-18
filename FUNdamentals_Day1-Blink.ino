3dfa1b6131c49bccab6402a3792847fc0600a08d
Krystal Bernal <krystalbc@sbcglobal.net>
2017-02-17 17:43:03 -0800

    Add files via upload

---
+++ FUNdamentals_Day1-Blink.ino
@@ -0,0 +1,19 @@
+/* Krystal Bernal
+ * Engineering FUNdamentals
+ * Day 1: Blink
+ * This program uses an LED
+ * Last updated: 02/26/16
+*/
+
+void setup() {
+  // initialize digital pin 13 as an output.
+  pinMode(13, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(13, HIGH); 
+  delay(1000);              
+  digitalWrite(13, LOW);   
+  delay(1000);
+}
